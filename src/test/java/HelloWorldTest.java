be0c952e8dcb36ce9857241133f459945a1bb772
Noah Reinhardt <ndrein@gmail.com>
2017-04-27 18:41:10 -0400

    Setup method

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,6 +1,7 @@
 package test.java;
 
 import main.java.HelloWorld;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,9 +10,15 @@ import static org.junit.jupiter.api.Assertions.*;
  * Created by ndrei on 2017-04-27.
  */
 class HelloWorldTest {
+    private HelloWorld hello;
+
+    @BeforeEach
+    void setUp() {
+        hello = new HelloWorld();
+    }
+
     @Test
     void hello() {
-        HelloWorld hello = new HelloWorld();
         assertEquals("hello", hello.hello());
     }
 }
