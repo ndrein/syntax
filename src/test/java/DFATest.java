9ee2fc8b6ba86330f6a011745c66160e7b3ed94e
Noah Reinhardt <ndrein@gmail.com>
2017-04-28 20:00:06 -0400

    Complex construction

--- src/test/java/DFATest.java
+++ src/test/java/DFATest.java
@@ -4,6 +4,7 @@ import com.google.common.collect.HashBasedTable;
 import main.java.DFA;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
@@ -16,5 +17,29 @@ class DFATest {
     void basicConstruction() {
         dfa = new DFA(new HashSet<>(), new HashSet<>(Collections.singletonList(1)),
                 1, new HashSet<>(Arrays.asList(1, 2)), HashBasedTable.create());
+    }
+
+    @Test
+    void complexConstruction() {
+        HashSet<Character> alphabet = new HashSet<>();
+        alphabet.add('a');
+        alphabet.add('b');
+        alphabet.add('c');
+
+        HashSet<Integer> states = new HashSet<>();
+        states.add(0);
+        states.add(1);
+        states.add(2);
+
+        Integer start_state = 0;
+
+        HashSet<Integer> accept_states = new HashSet<>();
+
+        HashBasedTable<Integer, Character, Integer> transitions = HashBasedTable.create();
+        transitions.put(0, 'a', 1);
+        transitions.put(1, 'c', 0);
+        transitions.put(2, 'b', 2);
+
+        dfa = new DFA(alphabet, states, start_state, accept_states, transitions);
     }
 }
