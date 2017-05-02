ad50c4ddbbf5b808d301ac10e448b36c89ee0bff
Noah Reinhardt <ndrein@gmail.com>
2017-05-02 17:53:15 -0400

    Add simple test

--- src/test/java/SimpleDFATest.java
+++ src/test/java/SimpleDFATest.java
@@ -1,8 +1,13 @@
 package test.java;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * Created by ndrei on 2017-05-02.
  */
 class SimpleDFATest extends DFATest {
-
+    @Test
+    void testConstructor() {
+        new SimpleFactory().generate();
+    }
 }
