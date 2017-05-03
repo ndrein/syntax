c9b9b9fef112ba484564b34b77a8a8fb69eb7696
Noah Reinhardt <ndrein@gmail.com>
2017-05-03 12:54:41 -0400

    Implement State

---
+++ src/main/java/State.java
@@ -0,0 +1,17 @@
+package main.java;
+
+/**
+ * Created by ndrei on 2017-05-03.
+ */
+public class State {
+    private Integer integer;
+
+    public State(Integer i) {
+        this.integer = i;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
