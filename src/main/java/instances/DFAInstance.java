9d560ca1945494d9db839f3c2422ba702cdabc9d
Noah Reinhardt <ndrein@gmail.com>
2017-05-10 08:10:00 -0400

    Remove instances

--- src/main/java/instances/DFAInstance.java
+++
@@ -1,9 +0,0 @@
-package main.java.instances;
-
-import main.java.DFA;
-
-abstract public class DFAInstance extends DFA implements DFAInstanceImplementation {
-    public DFAInstance() {
-        super(alphabet, states, start_state, accept_states, transitions);
-    }
-}
