26bd50e1981b97ce133f700262437e55cf174dc0
Noah Reinhardt <ndrein@gmail.com>
2018-01-27 14:54:50 -0500

    Remove unused code

--- src/test/java/InvalidDFAConstructionsTest.java
+++ src/test/java/InvalidDFAConstructionsTest.java
@@ -12,8 +12,6 @@ import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-//import org.junit.jupiter.api.Test;
-
 public class InvalidDFAConstructionsTest {
     @Test
     public void cantConstructWithEmptyStateSet() {
--- src/test/java/ValidDFAConstructionTest.java
+++ src/test/java/ValidDFAConstructionTest.java
@@ -3,13 +3,11 @@ package test.java;
 import main.java.generators.SimpleGenerator;
 import main.java.generators.TrivialGenerator;
 import org.junit.Test;
-//import org.junit.jupiter.api.Test;
 
 public class ValidDFAConstructionTest {
     @Test
     public void testConstructingTrivialDFA() {
         new TrivialGenerator().generate();
-//        new TrivialInstance();
     }
 
     @Test
