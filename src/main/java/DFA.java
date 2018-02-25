b2fb4de852d106b3728d7706a386386e8807564b
Noah Reinhardt <ndrein@gmail.com>
2018-02-24 19:30:48 -0500

    Minor refactor

--- src/main/java/DFA.java
+++ src/main/java/DFA.java
@@ -3,17 +3,17 @@ package main.java;
 import com.google.common.collect.Table;
 import main.java.exceptions.InvalidDFAInput;
 
-import java.util.HashSet;
+import java.util.Set;
 
 public class DFA {
-    final private HashSet<Character> alphabet;
-    final private HashSet<State> states;
+    final private Set<Character> alphabet;
+    final private Set<State> states;
     final private State start_state;
-    final private HashSet<State> accept_states;
+    final private Set<State> accept_states;
     final private Table<State, Character, State> transitions;
 
-    public DFA(HashSet<Character> alphabet, HashSet<State> states, State start_state,
-               HashSet<State> accept_states, Table<State, Character, State> transitions) {
+    public DFA(Set<Character> alphabet, Set<State> states, State start_state,
+               Set<State> accept_states, Table<State, Character, State> transitions) {
         DFAValidator.validate(alphabet, states, start_state, accept_states, transitions);
 
         this.alphabet = alphabet;
--- src/main/java/DFAValidator.java
+++ src/main/java/DFAValidator.java
@@ -3,29 +3,29 @@ package main.java;
 import com.google.common.collect.Table;
 import main.java.exceptions.*;
 
-import java.util.HashSet;
+import java.util.Set;
 
 class DFAValidator {
-    static void validate(HashSet<Character> alphabet, HashSet<State> states,
-                         State start_state, HashSet<State> accept_states,
+    static void validate(Set<Character> alphabet, Set<State> states,
+                         State start_state, Set<State> accept_states,
                          Table<State, Character, State> transitions) throws InvalidDFAConstruction {
         checkStartState(start_state, states);
         checkAcceptStates(accept_states, states);
         checkTransitions(transitions, states, alphabet);
     }
 
-    private static void checkStartState(State start_state, HashSet<State> states) {
+    private static void checkStartState(State start_state, Set<State> states) {
         if (!states.contains(start_state))
             throw new StartStateNotInStates();
     }
 
-    private static void checkAcceptStates(HashSet<State> accept_states, HashSet<State> states) {
+    private static void checkAcceptStates(Set<State> accept_states, Set<State> states) {
         if (!states.containsAll(accept_states))
             throw new AcceptStatesNotContainedInStates();
     }
 
     private static void checkTransitions(Table<State, Character, State> transitions,
-                                         HashSet<State> states, HashSet<Character> alphabet) {
+                                         Set<State> states, Set<Character> alphabet) {
         for (State q : states) {
             for (Character c : alphabet) {
                 checkTransition(q, c, transitions, states);
@@ -35,7 +35,7 @@ class DFAValidator {
 
     private static void checkTransition(State q, Character c,
                                         Table<State, Character, State> transitions,
-                                        HashSet<State> states) {
+                                        Set<State> states) {
         checkTransitionDefined(q, c, transitions);
         checkTransitionValid(q, c, transitions, states);
     }
@@ -49,7 +49,7 @@ class DFAValidator {
 
     private static void checkTransitionValid(State q, Character c,
                                              Table<State, Character, State> transitions,
-                                             HashSet<State> states) {
+                                             Set<State> states) {
         State next = transitions.get(q, c);
         if (!states.contains(next))
             throw new InvalidTransition(String.format("Transition %d x %c -> %d is invalid", q.getInt(), c, next.getInt()));
