7e90c388b46eb86029472bdea42c89af0b146c97
Noah Reinhardt <ndrein@gmail.com>
2018-02-24 19:37:21 -0500

    Use int instead of Integer in State

--- src/main/java/State.java
+++ src/main/java/State.java
@@ -1,24 +1,30 @@
 package main.java;
 
 
-public class State {
-    final private Integer integer;
+import java.util.Objects;
 
-    public State(Integer i) {
+public class State {
+    final private int integer;
+
+    public State(int i) {
         this.integer = i;
     }
 
-    public Integer getInt() {
+    public int getInt() {
         return integer;
     }
 
     @Override
-    public boolean equals(Object other) {
-        return other.getClass() == this.getClass() && ((State) other).integer.equals(integer);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        State state = (State) o;
+        return integer == state.integer;
     }
 
     @Override
     public int hashCode() {
-        return integer.hashCode();
+
+        return Objects.hash(integer);
     }
 }
